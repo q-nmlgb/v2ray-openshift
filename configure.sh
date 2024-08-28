aa282e4794a073ddb41fef7f9c46ccf3bb45c248
Tad Stiedemann <71119932+q-nmlgb@users.noreply.github.com>
2024-08-28 21:06:35 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 # V2Ray new configuration
-cat <<-EOF > /etc/v2ray/config.json
-$CONFIG_JSON
-EOF
+cat /etc/v2ray/config.json
 # Run V2Ray
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
